7225dfc1354dd27b43311749176fc91bf08ccd20
小蘑菇 <33219558+yuanmogu@users.noreply.github.com>
2022-10-18 19:14:32 +0800

    Update Addons.php

--- src/Addons.php
+++ src/Addons.php
@@ -9,11 +9,13 @@ declare(strict_types=1);
 namespace think;
 
 use think\App;
+use think\Response;
 use think\helper\Str;
 use think\facade\Config;
 use think\facade\View;
 use think\facade\Db;
 use think\view\driver\Think;
+use think\exception\HttpResponseException;
 
 
 abstract class Addons
@@ -51,12 +53,12 @@ abstract class Addons
 
         $this->view = new Think($app, config('view'));
         $this->view->config([
+			'tpl_cache' => false,
             'view_path' => $this->addon_path . 'view' . DIRECTORY_SEPARATOR,
-            'tpl_replace_string'=>[
-               '__ADDONS__' => '/static/addons/'.$this->name.'/'
-            ]
+			'tpl_replace_string'  =>  [
+				'__ADDONS__' => '/static/addons/'.$this->name.'/'
+			]
         ]);
-
 	
         // 控制器初始化
         $this->initialize();
@@ -64,7 +66,10 @@ abstract class Addons
 
     // 初始化
     protected function initialize()
-    {}
+    {
+
+	
+	}
 
 
     /**
@@ -130,6 +135,103 @@ abstract class Addons
         $this->view->engine($engine);
 
         return $this;
+    }
+
+
+
+    /**
+     * 操作成功跳转的快捷方法
+     * @access protected
+     * @param  mixed     $msg 提示信息
+     * @param  string    $url 跳转的URL地址
+     * @param  mixed     $data 返回的数据
+     * @param  integer   $wait 跳转等待时间
+     * @param  array     $header 发送的Header信息
+     * @return void
+     */
+    protected function success($msg = '操作成功！', string $url = null, $data = '',  int $code = 1, int $wait = 1, array $header = [])
+    {
+        if (is_null($url) && isset($_SERVER["HTTP_REFERER"])) {
+            $url = $_SERVER["HTTP_REFERER"];
+        } elseif ($url) {
+			$url = (strpos($url, '://') || 0 === strpos($url, '/') || 0 === strpos($url, 'javascript:')) ? $url : (string)$this->app->route->buildUrl($url);
+        }
+
+        $result = [
+            'code'  => $code,
+            'msg'   => $msg,
+            'data'  => $data,
+            'url'   => $url,
+            'wait'  => $wait,
+        ];
+
+		$type = ($this->request->isJson() || $this->request->isAjax()) ? 'json' : 'html';
+
+        if (strtolower($type) == 'html'){
+			$response = Response::create(config('app.dispatch_success_tmpl'), 'view')->assign($result)->header($header);
+        } else {
+			$response = Response::create($result, $type)->header($header);
+        }
+        
+        throw new HttpResponseException($response);
+    }
+
+    /**
+     * 操作错误跳转的快捷方法
+     * @access protected
+     * @param  mixed     $msg 提示信息
+     * @param  string    $url 跳转的URL地址
+     * @param  mixed     $data 返回的数据
+     * @param  integer   $wait 跳转等待时间
+     * @param  array     $header 发送的Header信息
+     * @return void
+     */
+    protected function error($msg = '操作失败！',  $url = null, $data = '', int $code = 0, int $wait = 3, array $header = [])
+    {
+        if (is_null($url)) {
+            $url = $this->request->isAjax() ? '' : 'javascript:history.back(-1);';
+        } elseif ($url) {
+			$url = (strpos($url, '://') || 0 === strpos($url, '/') || 0 === strpos($url, 'javascript:') ) ? $url : (string)$this->app->route->buildUrl($url);
+        }
+
+        $result = [
+            'code' => $code,
+            'msg'  => $msg,
+            'data' => $data,
+            'url'  => $url,
+            'wait' => $wait,
+        ];
+
+        $type = ($this->request->isJson() || $this->request->isAjax()) ? 'json' : 'html';
+        if ($type == 'html'){
+			$response = Response::create(config('app.dispatch_error_tmpl'), 'view')->assign($result)->header($header);
+        } else {
+			$response = Response::create($result, $type)->header($header);
+        }
+        throw new HttpResponseException($response);
+    }
+
+
+    /**
+     * URL重定向
+     * @access protected
+     * @param  string         $url 跳转的URL表达式
+     * @param  array|integer  $params 其它URL参数
+     * @param  integer        $code http code
+     * @param  array          $with 隐式传参
+     * @return void
+     */
+    protected function redirect($url, $params = [], $code = 302, $with = [])
+    {
+        $response = Response::create($url, 'redirect');
+
+        if (is_integer($params)) {
+            $code   = $params;
+            $params = [];
+        }
+     
+        $response->code($code);
+        throw new HttpResponseException($response);
     }
 
 
